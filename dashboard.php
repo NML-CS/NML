68c465de824002bd693b4170f5c950b0dbcddca6
NML-CS <151976769+NML-CS@users.noreply.github.com>
2023-12-28 19:25:38 -0500

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,3 @@
-<?php
-session_start();
-if(!$_SESSION['logged_in']){
-    header("location: error.php");
-    exit();
-}
-
-$username = $_SESSION['userData']['name'];
-$avatar = $_SESSION['userData']['avatar'];
-
-?>
 <!doctype html>
 <html>
 <head>
@@ -20,9 +9,7 @@ $avatar = $_SESSION['userData']['avatar'];
 <body>
     <div class="flex items-center justify-center h-screen bg-steam-lightGray text-white flex-col">
         <div class="text-2xl">Welcome to the dashboard,</div>
-        <div class="text-4xl mt-3 flex items-center font-medium">
-            <img src='<?php echo $avatar;?>' class="rounded-full w-12 h-12 mr-3"/>
-            <?php echo $username;?></div>
+        <div class="text-4xl mt-3 flex items-center font-medium"><img src='{{avatar_src}}' class="rounded-full w-12 h-12 mr-3"/>{{username}}</div>
         <a href="logout.php" class="text-sm mt-5">Logout</a>
     </div>
 
